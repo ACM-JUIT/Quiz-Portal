9bfc7fe85ca420845f66df719b169daab4d89438
Devansh Chaudhary <devanshchaudhary2002@gmail.com>
2021-06-26 12:28:17 +0530

    Fixed Typo

--- routes/quiz.js
+++ routes/quiz.js
@@ -13,7 +13,7 @@ function getQuestion(req, res, next) {
 
 function checkAnswer(req, res, next) {
     quizController.checkAnswer(req.body)
-    .then(ans => ans ? res.json(ans) : res.status(400).json({ message: 'Incorrect Answer' }))
+    .then(ans => ans ? res.json(ans) : res.status(400).json({ message: 'Internal Error' }))
     .catch(err => next(err));
 }
 
