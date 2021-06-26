8178a52950ac7d6c5f4511d40f5de73edc62067b
Devansh Chaudhary <devanshchaudhary2002@gmail.com>
2021-06-26 10:45:48 +0530

    added mcq questions

--- models/question.js
+++ models/question.js
@@ -3,10 +3,17 @@ const Schema = mongoose.Schema;
 
 const questionSchema = new Schema({
     questionIndex: { type: Number, unique: true, required: true },
+    questionType: {type: String, required: true},
     textQuestion: { 
-        question: { type: String, required: true },
-        description: { type: String, required: true },
-        answer: { type: String, required: true }
+        question: { type: String },
+        description: { type: String },
+        answer: { type: String }
+    },
+    mcqQuestion: { 
+        question: { type: String },
+        description: { type: String },
+        options:{ type: Array },
+        answer: { type: String }
     },
     createdDate: { type: Date, default: Date.now }
 });
@@ -24,15 +31,35 @@ questionSchema.set('toJSON', {
 questionSchema.methods = {
 
     getQuestion: function(ques_type) {
-        return {
-            "question": this.textQuestion.question,
-            "description": this.textQuestion.description
-        };
+        if(ques_type == "text"){
+            return {
+                "questionType": this.questionType,
+                "question": this.textQuestion.question,
+                "description": this.textQuestion.description
+            };
+        }
+
+        if(ques_type == "mcq"){
+            return {
+                "questionType": this.questionType,
+                "question": this.mcqQuestion.question,
+                "description": this.mcqQuestion.description,
+                "options": this.mcqQuestion.options
+            };
+        }
+      
     },
 
     checkAnswer: function(ques_type) {
-        return this.textQuestion.answer;
+        if(ques_type == "text"){
+            return this.textQuestion.answer;
+        }
+
+        if(ques_type == "mcq"){
+            return this.mcqQuestion.answer;
+        }
     }
+    
 }
 
 module.exports = mongoose.model('Question', questionSchema);
