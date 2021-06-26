1f1d3c276e6364ecffd7899cd6797102aa9b0ddb
Devansh Chaudhary <devanshchaudhary2002@gmail.com>
2021-06-26 10:49:51 +0530

    removed question type from quiz model
    added this data in the question model

--- models/quiz.js
+++ models/quiz.js
@@ -4,8 +4,7 @@ const Schema = mongoose.Schema;
 const quizSchema = new Schema({
     user_id: { type: String, unique: true, required: true },
     user_name: { type: String, unique: true, required: true },
-    current_question: { type: Number, default: 1 },
-    question_type: { type: String, default: "text" }
+    current_question: { type: Number, default: 1 }
 });
 
 quizSchema.set('toJSON', {
