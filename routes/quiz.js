49cfcc8115ac63d9bd9b51d80b8afd7a2a1dbba3
Devansh Chaudhary <devanshchaudhary2002@gmail.com>
2021-07-01 14:12:57 +0530

    Bug Fixes

--- routes/quiz.js
+++ routes/quiz.js
@@ -9,25 +9,25 @@ router.post('/skipQuestion', skipQuestion);
 
 function getQuestion(req, res, next) {
     quizController.getQuestion(req.body)
-        .then(ques => ques ? res.json(ques) : res.status(400).json({ message: 'Internal Error' }))
+        .then(ques => ques ? res.json(ques) : res.status(500).json({ message: 'Internal Error' }))
         .catch(err => next(err));
 }
 
 function checkAnswer(req, res, next) {
     quizController.checkAnswer(req.body)
-    .then(ans => ans ? res.json(ans) : res.status(400).json({ message: 'Internal Error' }))
+    .then(ans => ans ? res.json(ans) : res.status(500).json({ message: 'Internal Error' }))
     .catch(err => next(err));
 }
 
 function leaderboard(req, res, next) {
     quizController.leaderboard(req.body)
-    .then(users => users ? res.json(users) : res.status(400).json({ message: 'Internal Error' }))
+    .then(users => users ? res.json(users) : res.status(500).json({ message: 'Internal Error' }))
     .catch(err => next(err));
 }
 
 function skipQuestion(req, res, next) {
     quizController.skipQuestion(req.body)
-    .then(result => result ? res.json(result) : res.status(400).json({ message: 'Internal Error' }))
+    .then(result => result ? res.json(result) : res.status(500).json({ message: 'Internal Error' }))
     .catch(err => next(err));
 }
 
